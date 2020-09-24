7fdf9eecbd297e47ffaeb926fe7989522d02e67c
msierbin <msierbin@gmail.com>
2020-09-24 06:14:38 +0200

    Init - generated by Spring Initializr

---
+++ src/test/java/info/sierbin/nordea/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package info.sierbin.nordea.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
