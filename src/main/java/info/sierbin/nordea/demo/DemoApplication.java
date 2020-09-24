1f32b5ef92ae093c7b8955572d6211eca750062b
msierbin <msierbin@gmail.com>
2020-09-24 06:20:41 +0200

    Turn to standalone app. Turn off logs.

--- src/main/java/info/sierbin/nordea/demo/DemoApplication.java
+++ src/main/java/info/sierbin/nordea/demo/DemoApplication.java
@@ -1,13 +1,21 @@
 package info.sierbin.nordea.demo;
 
+import org.springframework.boot.Banner;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication app = new SpringApplication(DemoApplication.class);
+		app.setBannerMode(Banner.Mode.OFF);
+		app.run(args);
 	}
 
+	@Override
+	public void run(String... args) {
+		// intentionally empty
+	}
 }
